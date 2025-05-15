4f86be8517ac0a89c2a0bc0f503670565f0087a1
Larysa <seagull2610@gmail.com>
2025-05-15 14:20:41 +0200

    task 1-2

---
+++ src/middlewares/errorHandler.js
@@ -0,0 +1,17 @@
+import { isHttpError } from 'http-errors';
+
+export function errorHandler(error, req, res, next) {
+  if (isHttpError(error) === true) {
+    return res
+      .status(error.status)
+      .json({ status: error.status, message: error.message });
+  }
+
+  console.log(error);
+
+  res.status(500).json({
+    status: 500,
+    message: 'Something went wrong',
+    data: 'Internal server Error',
+  });
+}
